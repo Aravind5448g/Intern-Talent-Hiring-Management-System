a88dcc8f0fcf8279a2779579a4efff2634760c52
Aravind5448 <727724euai013@skcet.ac.in>
2025-12-17 16:39:04 +0000

    Day 5 commit done

--- springapp/src/main/java/com/examly/springapp/repository/JobApplicationRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/JobApplicationRepository.java
@@ -1,7 +1,7 @@
 package com.examly.springapp.repository;
 import com.examly.springapp.model.JobApplication;
-import org.springFramework.data.jpa.repository.JpaRepository;
-import org.springFramework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JobApplicationRepository extends JpaRepository <JobApplication , Long>{
